4ba25d63bd64a693e83a7da3225f3ce58428cf3d
Afolabibusayo <132835478+Afolabibusayo@users.noreply.github.com>
2023-06-24 11:56:18 -0700

    first commit

---
+++ iteration.js
@@ -0,0 +1,85 @@
+// for ( let i = 0; i <=10; i++){
+//     let result = 2 * i;
+//     console.log(result);
+// }
+
+// for ( let i = 0; i<=10; i++){
+//    console.log(i);
+// }
+
+// for ( let output = 1; output<=10; output++ ){
+//    console.log(output);
+// }
+
+
+// function numbers( p1){
+//   let value = p1;
+//   return value;
+  
+// }
+// console.log( numbers("BUSAYO"))
+
+//  function result(r){
+//   output = 3.142 * r**2;
+//   value = 2 * 3.142 * r; 
+//     return output, value;
+//  }
+//  console.log(result(2))
+//  console.log(result(4))
+//  console.log(result(6))
+
+// for statement examples......
+// const cars = ["BMW","Volvo","Saab","Ford","Fiat","Audi"]
+//  for ( let i = 0; i < cars.length; i++){
+//   let text = cars[i];
+//   console.log(text)
+//  }
+
+// for ( let i = 2; i <= 100; i++){
+//   if ( i % 2 !==0){
+//   console.log(i)
+//   }
+// }
+// let num = [2,3,5,8,7]
+// for ( let k = 0; k<num.length; k++){
+//     let result = num[k];
+//     console.log(result)
+// }
+
+// let fruits = ["Banana","orange","apple","guava"]
+// for ( let i = 2; i<=fruits.length;){
+//     let value = fruits[i];
+//     console.log(value)
+// }
+
+// // if, else, and else if statement examples..
+const hour = new Date()
+if ( 0<= hour < 11) {
+    console.log("Good Morning")
+} else if ( 12 <= hour < 18 ){ 
+    console.log("Good Day")
+} else{
+    console.log("Good Evening")
+}
+
+// // while, do while statement examples...
+// let i = 0;
+// while ( i < 10 ){
+//     i++;
+//     console.log(i)
+// }
+
+// const names = [ "Busayo","Adeoluwa","Adeola","Adekunle"]
+// let i = 0
+// while ( i <= 3){
+//     let value = names[i]
+//     i++
+//     console.log(value)
+// }
+
+// let i = 2
+// do {
+//     i++;
+//     console.log(i)
+// }
+// while( i < 10);
